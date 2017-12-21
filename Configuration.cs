bdc59ceb2e7f7025e3e5dda47a4018de3d8f277d
TheWhiteGuardian <twhiteguardian@gmail.com>
2017-12-21 19:32:46 +0100

    Add initial C# files

---
+++ Configuration.cs
@@ -0,0 +1,321 @@
+﻿using System;
+using UnityEngine;
+using KS3P.UnityPostProcessing;
+
+namespace KS3P.Configuration
+{
+    //These classes run when a scene is loaded.
+    //Depending on the scene, they load different profiles
+    //If data for the profile of a scene is missing, PP is disabled for that scene.
+    [KSPAddon(KSPAddon.Startup.Credits, false)]
+    public class Applier_Credits : MonoBehaviour
+    {
+        static bool Enabled;
+        //In Start, we check the status of the sister script in ProfileParser
+        void Start()
+        {
+            Debug.Log("[KSP_PostProcessing_Patcher]: Checking Profile Parser status...");
+            //If patched has been made true, we don't enter the while loop at all.
+            while(!ProfileParser.EffectApplier.patched)
+            {
+                Debug.Log("[KSP_PostProcessing]: Waiting for Profile Parser to finish parsing config.");
+                if (!ProfileParser.EffectApplier.processing && !ProfileParser.EffectApplier.patched)
+                {
+                    Debug.LogWarning("[KSP_PostProcessing]: Warning! Profile Parser seems to be inactive. Forcing patch...");
+                    ProfileParser.EffectApplier.Load();
+                }
+            }
+            Debug.Log("[KSP_PostProcessing]: Profile Parser greenlight received.");
+            Enabled = ProfileParser.EffectApplier.creditsExists;
+            //Effectively the while loop is there to stall until the ProfileParser is done.
+
+            //Therefore we get right down to business.
+            if (Enabled)
+            {
+                if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() == null) //If the camera has no post processing component
+                {
+                    Camera.main.gameObject.AddComponent<PostProcessingBehaviour>();
+                }
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().profile = ProfileParser.EffectApplier.Credits;
+            }
+            if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() != null)
+            {
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().enabled = Enabled;
+            }
+        }
+        
+    }
+
+    [KSPAddon(KSPAddon.Startup.EditorSPH, false)]
+    public class Applier_SPH : MonoBehaviour
+    {
+        static bool Enabled;
+        //In Start, we check the status of the sister script in ProfileParser
+        void Start()
+        {
+            Debug.Log("[KSP_PostProcessing_Patcher]: Checking Profile Parser status...");
+            //If patched has been made true, we don't enter the while loop at all.
+            while (!ProfileParser.EffectApplier.patched)
+            {
+                Debug.Log("[KSP_PostProcessing]: Waiting for Profile Parser to finish parsing config.");
+                if (!ProfileParser.EffectApplier.processing && !ProfileParser.EffectApplier.patched)
+                {
+                    Debug.LogWarning("[KSP_PostProcessing]: Warning! Profile Parser seems to be inactive. Forcing patch...");
+                    ProfileParser.EffectApplier.Load();
+                }
+            }
+            Debug.Log("[KSP_PostProcessing]: Profile Parser greenlight received.");
+            Enabled = ProfileParser.EffectApplier.SPHExists;
+            //Effectively the while loop is there to stall until the ProfileParser is done.
+
+            //Therefore we get right down to business.
+            if (Enabled)
+            {
+                if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() == null) //If the camera has no post processing component
+                {
+                    Camera.main.gameObject.AddComponent<PostProcessingBehaviour>();
+                }
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().profile = ProfileParser.EffectApplier.SPH;
+            }
+            if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() != null)
+            {
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().enabled = Enabled;
+            }
+        }
+
+    }
+
+    [KSPAddon(KSPAddon.Startup.EditorVAB, false)]
+    public class Applier_VAB : MonoBehaviour
+    {
+        static bool Enabled;
+        //In Start, we check the status of the sister script in ProfileParser
+        void Start()
+        {
+            Debug.Log("[KSP_PostProcessing_Patcher]: Checking Profile Parser status...");
+            //If patched has been made true, we don't enter the while loop at all.
+            while (!ProfileParser.EffectApplier.patched)
+            {
+                Debug.Log("[KSP_PostProcessing]: Waiting for Profile Parser to finish parsing config.");
+                if (!ProfileParser.EffectApplier.processing && !ProfileParser.EffectApplier.patched)
+                {
+                    Debug.LogWarning("[KSP_PostProcessing]: Warning! Profile Parser seems to be inactive. Forcing patch...");
+                    ProfileParser.EffectApplier.Load();
+                }
+            }
+            Debug.Log("[KSP_PostProcessing]: Profile Parser greenlight received.");
+            Enabled = ProfileParser.EffectApplier.VABExists;
+            //Effectively the while loop is there to stall until the ProfileParser is done.
+
+            //Therefore we get right down to business.
+            if (Enabled)
+            {
+                if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() == null) //If the camera has no post processing component
+                {
+                    Camera.main.gameObject.AddComponent<PostProcessingBehaviour>();
+                }
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().profile = ProfileParser.EffectApplier.VAB;
+            }
+            if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() != null)
+            {
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().enabled = Enabled;
+            }
+        }
+
+    }
+
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class Applier_Flight : MonoBehaviour
+    {
+        static bool Enabled;
+        //In Start, we check the status of the sister script in ProfileParser
+        void Start()
+        {
+            Debug.Log("[KSP_PostProcessing_Patcher]: Checking Profile Parser status...");
+            //If patched has been made true, we don't enter the while loop at all.
+            while (!ProfileParser.EffectApplier.patched)
+            {
+                Debug.Log("[KSP_PostProcessing]: Waiting for Profile Parser to finish parsing config.");
+                if (!ProfileParser.EffectApplier.processing && !ProfileParser.EffectApplier.patched)
+                {
+                    Debug.LogWarning("[KSP_PostProcessing]: Warning! Profile Parser seems to be inactive. Forcing patch...");
+                    ProfileParser.EffectApplier.Load();
+                }
+            }
+            Debug.Log("[KSP_PostProcessing]: Profile Parser greenlight received.");
+            Enabled = ProfileParser.EffectApplier.FlightExists;
+            //Effectively the while loop is there to stall until the ProfileParser is done.
+
+            //Therefore we get right down to business.
+            if (Enabled)
+            {
+                if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() == null) //If the camera has no post processing component
+                {
+                    Camera.main.gameObject.AddComponent<PostProcessingBehaviour>();
+                }
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().profile = ProfileParser.EffectApplier.Flight;
+            }
+            if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() != null)
+            {
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().enabled = Enabled;
+            }
+        }
+
+    }
+
+    [KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    public class Applier_MainMenu : MonoBehaviour
+    {
+        static bool Enabled;
+        //In Start, we check the status of the sister script in ProfileParser
+        void Start()
+        {
+            Debug.Log("[KSP_PostProcessing_Patcher]: Checking Profile Parser status...");
+            //If patched has been made true, we don't enter the while loop at all.
+            while (!ProfileParser.EffectApplier.patched)
+            {
+                Debug.Log("[KSP_PostProcessing]: Waiting for Profile Parser to finish parsing config.");
+                if (!ProfileParser.EffectApplier.processing && !ProfileParser.EffectApplier.patched)
+                {
+                    Debug.LogWarning("[KSP_PostProcessing]: Warning! Profile Parser seems to be inactive. Forcing patch...");
+                    ProfileParser.EffectApplier.Load();
+                }
+            }
+            Debug.Log("[KSP_PostProcessing]: Profile Parser greenlight received.");
+            Enabled = ProfileParser.EffectApplier.MMExists;
+            //Effectively the while loop is there to stall until the ProfileParser is done.
+
+            //Therefore we get right down to business.
+            if (Enabled)
+            {
+                if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() == null) //If the camera has no post processing component
+                {
+                    Camera.main.gameObject.AddComponent<PostProcessingBehaviour>();
+                }
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().profile = ProfileParser.EffectApplier.MainMenu;
+            }
+            if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() != null)
+            {
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().enabled = Enabled;
+            }
+        }
+
+    }
+
+    [KSPAddon(KSPAddon.Startup.Settings, false)]
+    public class Applier_Settings : MonoBehaviour
+    {
+        static bool Enabled;
+        //In Start, we check the status of the sister script in ProfileParser
+        void Start()
+        {
+            Debug.Log("[KSP_PostProcessing_Patcher]: Checking Profile Parser status...");
+            //If patched has been made true, we don't enter the while loop at all.
+            while (!ProfileParser.EffectApplier.patched)
+            {
+                Debug.Log("[KSP_PostProcessing]: Waiting for Profile Parser to finish parsing config.");
+                if (!ProfileParser.EffectApplier.processing && !ProfileParser.EffectApplier.patched)
+                {
+                    Debug.LogWarning("[KSP_PostProcessing]: Warning! Profile Parser seems to be inactive. Forcing patch...");
+                    ProfileParser.EffectApplier.Load();
+                }
+            }
+            Debug.Log("[KSP_PostProcessing]: Profile Parser greenlight received.");
+            Enabled = ProfileParser.EffectApplier.SettingsExists;
+            //Effectively the while loop is there to stall until the ProfileParser is done.
+
+            //Therefore we get right down to business.
+            if (Enabled)
+            {
+                if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() == null) //If the camera has no post processing component
+                {
+                    Camera.main.gameObject.AddComponent<PostProcessingBehaviour>();
+                }
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().profile = ProfileParser.EffectApplier.Settings;
+            }
+            if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() != null)
+            {
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().enabled = Enabled;
+            }
+        }
+
+    }
+
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    public class Applier_KSC : MonoBehaviour
+    {
+        static bool Enabled;
+        //In Start, we check the status of the sister script in ProfileParser
+        void Start()
+        {
+            Debug.Log("[KSP_PostProcessing_Patcher]: Checking Profile Parser status...");
+            //If patched has been made true, we don't enter the while loop at all.
+            while (!ProfileParser.EffectApplier.patched)
+            {
+                Debug.Log("[KSP_PostProcessing]: Waiting for Profile Parser to finish parsing config.");
+                if (!ProfileParser.EffectApplier.processing && !ProfileParser.EffectApplier.patched)
+                {
+                    Debug.LogWarning("[KSP_PostProcessing]: Warning! Profile Parser seems to be inactive. Forcing patch...");
+                    ProfileParser.EffectApplier.Load();
+                }
+            }
+            Debug.Log("[KSP_PostProcessing]: Profile Parser greenlight received.");
+            Enabled = ProfileParser.EffectApplier.KSCExists;
+            //Effectively the while loop is there to stall until the ProfileParser is done.
+
+            //Therefore we get right down to business.
+            if (Enabled)
+            {
+                if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() == null) //If the camera has no post processing component
+                {
+                    Camera.main.gameObject.AddComponent<PostProcessingBehaviour>();
+                }
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().profile = ProfileParser.EffectApplier.KSC;
+            }
+            if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() != null)
+            {
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().enabled = Enabled;
+            }
+        }
+
+    }
+
+    [KSPAddon(KSPAddon.Startup.TrackingStation, false)]
+    public class Applier_TR : MonoBehaviour
+    {
+        static bool Enabled;
+        //In Start, we check the status of the sister script in ProfileParser
+        void Start()
+        {
+            Debug.Log("[KSP_PostProcessing_Patcher]: Checking Profile Parser status...");
+            //If patched has been made true, we don't enter the while loop at all.
+            while (!ProfileParser.EffectApplier.patched)
+            {
+                Debug.Log("[KSP_PostProcessing]: Waiting for Profile Parser to finish parsing config.");
+                if (!ProfileParser.EffectApplier.processing && !ProfileParser.EffectApplier.patched)
+                {
+                    Debug.LogWarning("[KSP_PostProcessing]: Warning! Profile Parser seems to be inactive. Forcing patch...");
+                    ProfileParser.EffectApplier.Load();
+                }
+            }
+            Debug.Log("[KSP_PostProcessing]: Profile Parser greenlight received.");
+            Enabled = ProfileParser.EffectApplier.TSExists;
+            //Effectively the while loop is there to stall until the ProfileParser is done.
+
+            //Therefore we get right down to business.
+            if (Enabled)
+            {
+                if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() == null) //If the camera has no post processing component
+                {
+                    Camera.main.gameObject.AddComponent<PostProcessingBehaviour>();
+                }
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().profile = ProfileParser.EffectApplier.TS;
+            }
+            if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() != null)
+            {
+                Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().enabled = Enabled;
+            }
+        }
+
+    }
+}
---
+++ ProfileParser.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using UnityEngine;
+using KS3P.UnityPostProcessing;
+using KS3P.Processor;
+using System.Collections.Generic;
+
+
+namespace KS3P.ProfileParser
+{
+    
+
+    //This class combines all of the code, essentially this class forms the bridge between the Unity Asset Bundle and Kerbal Space Program
+
+    //This code loads the profile. It loads only once, the first time the game reaches the main menu
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class EffectApplier : MonoBehaviour
+    {
+        public static ConfigNode configFile;
+
+        //This will store all profiles loaded from the config file
+        static ConfigNode[] profileArray;
+
+        //Credits
+        //SpacePlaneHangar
+        //VAB
+        //Flight
+        //MainMenu
+        //Settings
+        //KSC
+        //Trackingstation
+        public static List<ConfigNode> ProfileList = new List<ConfigNode>(); //Store all profiles in a dictionary
+        public static PostProcessingProfile Credits;
+        public static PostProcessingProfile SPH;
+        public static PostProcessingProfile VAB;
+        public static PostProcessingProfile Flight;
+        public static PostProcessingProfile MainMenu;
+        public static PostProcessingProfile Settings;
+        public static PostProcessingProfile KSC;
+        public static PostProcessingProfile TS;
+        public static bool patched = false; //False on startup, will permanently become true once the profiles have been patched.
+        public static bool processing = false; //This bool shows whether or not the Profile Parser is busy.
+        public static bool creditsExists, SPHExists, VABExists, FlightExists, MMExists, SettingsExists, KSCExists, TSExists; //This is for disabling PP for scenes for which there is no data.
+        public static bool loadedCFG = false, loadedShaders = false;
+        //In the Start function, we intialize
+        void Start()
+        {
+            processing = true; //To avoid other initalizers from calling the void while this class is already processing it.
+            if (!patched) { Load(); } //To avoid double-loading if another script took over
+            else
+            {
+                processing = false; //Another script did the job for us? Then skip.
+            }
+        }
+        
+        public static void Load()
+        {
+            Debug.Log("[KSP_PostProcessing_ProfileParser]: Beginning parsing operation.");
+            //Load the config file
+            if (!loadedCFG) //If the CFG has been loaded already, skip
+            {
+                Debug.Log("[KSP_PostProcessing_ProfileParser]: Loading config file...");
+                configFile = ConfigNode.Load(KSPUtil.ApplicationRootPath + "/GameData/KS3P/Config.cfg");
+                Debug.Log("[KSP_PostProcessing_ProfileParser]: Config file loaded.");
+                loadedCFG = true;
+            }
+
+            //Load the shaders
+            if (!loadedShaders) //If the shaders have been loaded already, skip
+            {
+                Debug.Log("[KSP_PostProcessing_ProfileParser]: Loading shaders...");
+                ShaderLoader.ShaderLoader.LoadAssetBundle("KS3P/Shaders", "postprocessingshaders");
+                Debug.Log("[KSP_PostProcessing_ProfileParser]: Shaders loaded.");
+            }
+
+            //Replacing Shader.Find in the Unity MaterialFactory with the external shader loading stuff should fix things up nicely. :)
+
+            //Store all profiles
+
+            Debug.Log("[KSP_PostProcessing_ProfileParser]: Initializing profile...");
+
+            Debug.Log("[KSP_PostProcessing_ProfileParser]: Loading profiles...");
+            profileArray = configFile.GetNodes("Post_Processing");
+            Debug.Log("[KSP_PostProcessing_ProfileParser]: Found [" + profileArray.Length + "] profiles.");
+
+
+            //Add profiles
+            Debug.Log("[KSP_PostProcessing_ProfileParser]: Indexing profiles...");
+            foreach (ConfigNode profile in profileArray)
+            {
+                ProfileList.Add(profile);
+                Debug.Log("[KSP_PostProcessing_ProfileParser]: Adding profile for scene: [" + profile.GetValue("Scene") + "].");
+            }
+
+            Debug.Log("[KSP_PostProcessing_ProfileParser]: Initializing Post-Processing profiles...");
+
+            Debug.Log("[KSP_PostProcesing_ProfileParser]: Generating profile for scene [Credits]...");
+            ProfileProcessor.LoadProfile(out Credits, ProfileList, "Credits", out creditsExists);
+            if (!creditsExists) { Debug.LogWarning("[KSP_PostProcessing_ProfileParser]: Warning! No data found for scene [Credits], disabling Post-Processing for this scene!"); }
+
+            Debug.Log("[KSP_PostProcesing_ProfileParser]: Generating profile for scene [SpacePlaneHangar]...");
+            ProfileProcessor.LoadProfile(out SPH, ProfileList, "SpacePlaneHangar", out SPHExists);
+            if (!SPHExists) { Debug.LogWarning("[KSP_PostProcessing_ProfileParser]: Warning! No data found for scene [SpacePlaneHangar], disabling Post-Processing for this scene!"); }
+
+            Debug.Log("[KSP_PostProcesing_ProfileParser]: Generating profile for scene [VehicleAssemblyBuilding]...");
+            ProfileProcessor.LoadProfile(out VAB, ProfileList, "VehicleAssemblyBuilding", out VABExists);
+            if (!VABExists) { Debug.LogWarning("[KSP_PostProcessing_ProfileParser]: Warning! No data found for scene [VehicleAssemblyBuilding], disabling Post-Processing for this scene!"); }
+
+            Debug.Log("[KSP_PostProcesing_ProfileParser]: Generating profile for scene [Flight]...");
+            ProfileProcessor.LoadProfile(out Flight, ProfileList, "Flight", out FlightExists);
+            if (!FlightExists) { Debug.LogWarning("[KSP_PostProcessing_ProfileParser]: Warning! No data found for scene [Flight], disabling Post-Processing for this scene!"); }
+
+            Debug.Log("[KSP_PostProcesing_ProfileParser]: Generating profile for scene [MainMenu]...");
+            ProfileProcessor.LoadProfile(out MainMenu, ProfileList, "MainMenu", out MMExists);
+            if (!MMExists) { Debug.LogWarning("[KSP_PostProcessing_ProfileParser]: Warning! No data found for scene [MainMenu], disabling Post-Processing for this scene!"); }
+
+            Debug.Log("[KSP_PostProcesing_ProfileParser]: Generating profile for scene [Settings]...");
+            ProfileProcessor.LoadProfile(out Settings, ProfileList, "Settings", out SettingsExists);
+            if (!SettingsExists) { Debug.LogWarning("[KSP_PostProcessing_ProfileParser]: Warning! No data found for scene [Settings], disabling Post-Processing for this scene!"); }
+
+            Debug.Log("[KSP_PostProcesing_ProfileParser]: Generating profile for scene [KerbalSpaceCenter]...");
+            ProfileProcessor.LoadProfile(out KSC, ProfileList, "KerbalSpaceCenter", out KSCExists);
+            if (!KSCExists) { Debug.LogWarning("[KSP_PostProcessing_ProfileParser]: Warning! No data found for scene [KerbalSpaceCenter], disabling Post-Processing for this scene!"); }
+
+            Debug.Log("[KSP_PostProcesing_ProfileParser]: Generating profile for scene [TrackingStation]...");
+            ProfileProcessor.LoadProfile(out TS, ProfileList, "TrackingStation", out TSExists);
+            if (!TSExists) { Debug.LogWarning("[KSP_PostProcessing_ProfileParser]: Warning! No data found for scene [TrackingStation], disabling Post-Processing for this scene!"); }
+
+            processing = false;
+            Debug.Log("[KSP_PostProcessing_ProfileParser]: Finished initializing profile.");
+            patched = true; //We finished patching.
+            //GameEvents.onLevelWasLoaded.Add(new EventData<GameScenes>.OnEvent(OnSceneChange));
+        }
+
+
+
+
+        /*
+       /// <summary>
+       /// Executed when KSP changes a scene
+       /// </summary>
+       /// <param name="scene">The loaded scene</param>
+       static void OnSceneChange(GameScenes scene)
+       {
+           //Pick depending on the scene
+           switch (scene)
+           {
+               case GameScenes.LOADING:
+                   PatchCamera(loading);
+                   break;
+               case GameScenes.LOADINGBUFFER:
+                   PatchCamera(loadingBuffer);
+                   break;
+               case GameScenes.MAINMENU:
+                   PatchCamera(mainMenu);
+                   break;
+               case GameScenes.SETTINGS:
+                   PatchCamera(settings);
+                   break;
+               case GameScenes.CREDITS:
+                   PatchCamera(credits);
+                   break;
+               case GameScenes.SPACECENTER:
+                   PatchCamera(ksc);
+                   break;
+               case GameScenes.EDITOR:
+                   PatchCamera(editor);
+                   break;
+               case GameScenes.FLIGHT:
+                   PatchCamera(flight);
+                   break;
+               case GameScenes.TRACKSTATION:
+                   PatchCamera(trackstation);
+                   break;
+               case GameScenes.PSYSTEM:
+                   PatchCamera(psystem);
+                   break;
+           }
+       }
+
+       #region EnabledScenes
+       static bool loading = false;
+       static bool loadingBuffer = false;
+       static bool mainMenu = false;
+       static bool settings = false;
+       static bool credits = false;
+       static bool ksc = false;
+       static bool editor = false;
+       static bool flight = false;
+       static bool trackstation = false;
+       static bool psystem = false;
+       #endregion
+
+
+       static void PatchCamera(bool add)
+       {
+           if (Camera.main.gameObject.GetComponent<PostProcessingBehaviour>() == null)
+           {
+               Camera.main.gameObject.AddComponent<PostProcessingBehaviour>();
+           }
+
+           Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().profile = profile;
+           Camera.main.gameObject.GetComponent<PostProcessingBehaviour>().enabled = add;
+       }
+       */
+    }
+}
